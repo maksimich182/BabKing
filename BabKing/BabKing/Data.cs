0bfd31a7a39df9731798f3ebdd10d3902ed8617e
maksimich182 <66036049+maksimich182@users.noreply.github.com>
2022-02-04 18:40:40 +0300

    Структура данных
    Добавлен класс для исходных данных

---
+++ BabKing/BabKing/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabKing
+{
+    class Data
+    {
+        private byte[] _arData;
+
+        public byte[] ArData
+        {
+            get
+            {
+                return _arData;
+            }
+            set
+            {
+                _arData = value;
+            }
+        }
+
+        public void GetDataFromFile(string strFileName)
+        {
+            try
+            {
+                _arData = File.ReadAllBytes(strFileName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
